eda3e9590bf4d982637fa1ad3debf6ef69f6087f
pathetic bitch <vewarisking575@gmail.com>
2026-03-21 05:34:09 +0700

    Update Loader.lua

--- ND_V1/Loader.lua
+++ ND_V1/Loader.lua
@@ -2,7 +2,7 @@
 task.spawn(function()
     loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a6ec09360d70bac5f1aedc895ee03384.lua"))()
 end)
-task.wait(15)
+task.wait(20)
 task.spawn(function()
     loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/73b23cf3bbbcb6c13fd073fd3165eff7.lua"))()
 end)
