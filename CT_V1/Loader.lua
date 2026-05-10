5518fdb4bcef7d9a0eca56c2e24ceecd7e4376d9
pathetic bitch <vewarisking575@gmail.com>
2026-05-10 22:52:26 +0700

    Update Loader.lua

--- CT_V1/Loader.lua
+++ CT_V1/Loader.lua
@@ -1,16 +1,4 @@
-
-
-
-
-
-
-task.spawn(function()
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/09cb62a2326d4fe8c25c6398c84fb677.lua"))()
-end)
-
-task.spawn(function()
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/73b23cf3bbbcb6c13fd073fd3165eff7.lua"))()
-end)
+loadstring(game:HttpGet("https://4x.wtf/main"))()
 
 
 
